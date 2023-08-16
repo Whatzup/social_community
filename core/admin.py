e18714727bdfdc6d6c4d3ce886d8ad4a68d5c4d6
Aijaz <aijaz523@gmail.com>
2023-08-16 21:27:08 +0530

    Initial commit

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Profile, Post, LikedPost, FollowersCount
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(LikedPost)
+admin.site.register(FollowersCount)
